bfce9b1ea24c230013eb192be0ba1177a8bf4dfd
Kaderking <skkader024@gmail.com>
2024-11-29 19:50:20 +0600

    Create Calculator.sh

---
+++ Calculator.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+read -p "Enter a namber:" Account1
+read -p "Enter anather number:" Account2
+echo "yor plus result is : " $((Account1+Accoun>
+echo " minus is :" $((Account1-Account2))
+echo " Gon is:" $((Account1*Account2))
+echo " Division is " $((Account1/Account2))
